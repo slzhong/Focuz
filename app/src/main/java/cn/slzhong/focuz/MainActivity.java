270b0cee3428d7a993ba76902e645654e87b6f10
SherlockZhong <sherlock.z0917@gmail.com>
2015-10-04 12:56:04 +0800

    finished expand animation

--- app/src/main/java/cn/slzhong/focuz/MainActivity.java
+++ app/src/main/java/cn/slzhong/focuz/MainActivity.java
@@ -1,10 +1,21 @@
 package cn.slzhong.focuz;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * MainActivity
@@ -13,12 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
     private FrameLayout root;
 
+    private RelativeLayout welcome;
+    private TextView welcomeTitle;
+    private LinearLayout welcomeBackground;
+
+    private Handler animationHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initViews();
+        initData();
+        showAnimations();
     }
 
     @Override
@@ -41,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
                 enterFullscreen();
             }
         });
+        welcome = (RelativeLayout) findViewById(R.id.rl_welcome);
+        welcomeTitle = (TextView) findViewById(R.id.tv_welcome_title);
+        welcomeBackground = (LinearLayout) findViewById(R.id.ll_welcome_background);
 
         // hide actionbar
         ActionBar actionBar = getSupportActionBar();
@@ -52,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
         enterFullscreen();
     }
 
+    private void initData() {
+        animationHandler = new Handler();
+    }
+
     private void enterFullscreen() {
         root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
@@ -59,6 +85,65 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private void showAnimations() {
+        showWelcomeAnimation();
+
+        animationHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showCopyrightAnimation();
+            }
+        }, 2000);
+    }
+
+    private void showWelcomeAnimation() {
+        AnimationSet animationSet = new AnimationSet(false);
+        animationSet.setFillEnabled(true);
+        animationSet.setFillAfter(true);
+
+        TranslateAnimation moveUp = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, -4);
+        moveUp.setDuration(600);
+        moveUp.setStartOffset(100);
+        moveUp.setInterpolator(new DecelerateInterpolator());
+        animationSet.addAnimation(moveUp);
+
+        TranslateAnimation dropDown = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, 3.5f);
+        dropDown.setDuration(525);
+        dropDown.setStartOffset(800);
+        dropDown.setInterpolator(new AccelerateInterpolator());
+        animationSet.addAnimation(dropDown);
+
+        TranslateAnimation expandTranslate = new TranslateAnimation(
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, -0.48f,
+                Animation.RELATIVE_TO_SELF, 0,
+                Animation.RELATIVE_TO_SELF, -0.2f);
+        expandTranslate.setDuration(200);
+        expandTranslate.setStartOffset(1325);
+        expandTranslate.setInterpolator(new LinearInterpolator());
+        animationSet.addAnimation(expandTranslate);
+
+        ScaleAnimation expandScale = new ScaleAnimation(1, 20, 1, 20);
+        expandScale.setDuration(300);
+        expandScale.setStartOffset(1325);
+        expandScale.setInterpolator(new LinearInterpolator());
+        animationSet.addAnimation(expandScale);
+
+        welcomeBackground.startAnimation(animationSet);
+    }
+
+    private void showCopyrightAnimation() {
+
     }
 
 }
